0a9021f449a0499e714b3c422417688321be222f
Andrew Parmet <andrew@parmet.com>
2022-10-20 00:36:09 -0400

    publish 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,18 +56,7 @@ data class Version(
 
 fun versionsToPublish(): List<Version> =
     listOf(
-        "1.0.0",
-        "1.1.0",
-        "1.1.1",
-        "1.2.0",
-        "1.2.1",
-        "1.3.0",
-        "1.3.1",
-        "1.4.0",
-        "1.5.0",
-        "1.6.0",
-        "1.7.0",
-        "1.8.0"
+        "1.9.0"
     ).flatMap { version ->
         listOf(Os.MAC, Os.WINDOWS, Os.LINUX).flatMap { os ->
             os.architectures.map { arch ->
